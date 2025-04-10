a8105826c321a754418b37a2912d5de68f97ff42
shoxrux0107 <s.shuxratov@newuu.uz>
2025-04-10 21:40:43 +0500

    upd

---
+++ LAB 13/src/test/java/com/example/LAB13/LAB13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LAB13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgLab13ApplicationTests {
+
+    @Tests
+    void contextLoads() {
+    }
+
+}
